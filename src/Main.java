bc9cdb69fa00e086b898723e5faf0cd07b19e908
Gylnaz <gulnazhati5@gmail.com>
2025-10-16 22:26:57 +0300

    MyFirstProject

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,20 @@ public class Main {
         int spices = 10; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println("Неправильные результаты:");
         System.out.println((water / eaters) + " миллилитров(а) воды");
         System.out.println((potatoes / eaters) + " картофелин(а/ы)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
+
+        System.out.println("Если сложить все порции обратно, получим:");
+        System.out.println((double)water / eaters + " миллилитров(а) воды");
+        System.out.println((double)potatoes / eaters + " картофелин(а/ы)");
+        System.out.println((double)chicken / eaters + " куриных(ое) бёдер(ро)");
+        System.out.println((double)spices / eaters + " ложек(ки/ка) специй");
+
+
+        System.out.println("1 куриное бедро потеряно");
     }
 }
